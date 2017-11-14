0123be050490181d3b832cea072520522137e0fe
Philip Edenborg <edenborg@f7204-11.f7204.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-14 11:15:03 +0100

    Detta gjorde vi under förmiddagen den fjortonde november

--- H1/MD2_main.c
+++ H1/MD2_main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -9,15 +10,21 @@
 
 #define PI 3.141592653589
 #define k_B 0.000086173303
-#define nbr_of_timesteps 1000 /* nbr_of_timesteps+1 = power of 2, for best speed */
+#define nbr_of_timesteps_eq 10000 /* nbr_of_timesteps+1 = power of 2, for best speed */
+#define nbr_of_timesteps 100
 #define nbr_of_atoms 256
-void time_scaling(double T_eq, double time, double timestep,  double *v[][3], double tau_TO);
 
-void time_scaling(double T_eq, double T, double timestep,  double *v[][3], double tau_T){
-  double alpha_T = 1 + timestep / tau_T * (T_eq - T) / T;
-  v = sqrt(alpha_T) * v;
+void temp_scale(double v[][3], double n, double T_eq, double tau_T, double T, double timestep)
+{
+  double alpha;
+  alpha = 1 + timestep/tau_T*(T_eq - T)/T;
+  for(int i = 0; i < n; i ++) {
+    for( int j = 0; j < 3; j++ ) {
+      v[i][j] = alpha * v[i][j];  
+    }
+  }
 }
-  
+
 /* Main program */
 int main()
 { 
@@ -33,7 +40,7 @@ int main()
 
   /* declare file variable */
   FILE *energy_file;
-  FILE *temperature_file;
+  FILE *temp_file;
 
   /* displacement, velocity and acceleration */
  	   
@@ -42,9 +49,9 @@ int main()
   double (*v)[3] = malloc(sizeof(double[nbr_of_atoms][3]));
   double (*F)[3] = malloc(sizeof(double[nbr_of_atoms][3]));
   double *E_pot = malloc(nbr_of_timesteps * sizeof(double));
+  double *E_kin_eq = malloc(nbr_of_timesteps_eq * sizeof(double));
   double *E_kin = malloc(nbr_of_timesteps * sizeof(double));
-  double *Temp = malloc(nbr_of_timesteps * sizeof(double));
-
+  double *temp = malloc(nbr_of_timesteps_eq * sizeof(double));
   
 
   /* Set variables */
@@ -54,8 +61,8 @@ int main()
   n_cell = 4;
   m = 27*1.0364*0.0001;
   tot_length  =  cell_length*n_cell;
-  T_eq = 700+273.15;
-  tau_T = timestep * 100;
+  T_eq = 500 + 273.15;
+  tau_T = 100 * timestep;
 
   /* Initialize lattice*/
   init_fcc(positions, n_cell, cell_length);
@@ -78,12 +85,33 @@ int main()
 	  v[i][j] = 0;
 	}
     }
-  /* Set initial displacements and velocites */
-
+  
+  /* kuk hålet */
   /* Calculate initial accelerations (forces)  based on initial displacements */
   get_forces_AL(F, positions, tot_length, nbr_of_atoms); 
   E_pot[0] = get_energy_AL(positions, tot_length, nbr_of_atoms);
   
+  /* timesteps according to velocity Verlet algorithm */
+  for (int i = 1; i < nbr_of_timesteps_eq + 1; i++) {
+    for (int x = 0; x < 3; x++) { // For three space directions 
+      for (int j = 0; j < nbr_of_atoms; j++) {
+	v[j][x] += timestep * 0.5 * F[j][x]/m; // v(t+0.5*dt)
+	positions[j][x] += timestep * v[j][x];  // q(t+dt)
+      }      
+    }
+    // a(t+dt)
+    get_forces_AL(F, positions, tot_length, nbr_of_atoms);
+    for(int j = 0; j < nbr_of_atoms; j++) {
+      for(int x = 0; x < 3; x++){
+	v[j][x] += timestep * 0.5 * F[j][x]/m; // v(t+dt)
+	E_kin_eq[i] += m * v[j][x] * v[j][x]*0.5;
+      }
+    }
+    temp[i] = 2 / (3 * nbr_of_atoms * k_B) * E_kin_eq[i];
+    temp_scale(v, nbr_of_atoms, T_eq, tau_T, temp[i], timestep);
+  }
+
+  /* Simulation */
   /* timesteps according to velocity Verlet algorithm */
   for (int i = 1; i < nbr_of_timesteps + 1; i++) {
     for (int x = 0; x < 3; x++) { // For three space directions 
@@ -101,24 +129,21 @@ int main()
       }
     }
     E_pot[i] = get_energy_AL(positions, tot_length, nbr_of_atoms);
-    /* Calculate instantaneous temperature using equipartition theorem */ 
-    Temp[i] = 2 / (3*nbr_of_atoms*k_B) * E_kin[i];
-    /* Temperature scaling */
-    for(int j = 0; j < nbr_of_atoms; j++) {
-    time_scaling(T_eq, i*timestep, timestep, v[j][], tau_T);
-    }
-
   }
   
-  /* Print temperature and energy data to output file */
+  /* Print displacement and energy data to output file */
   energy_file = fopen("energy.dat","w");
+  temp_file = fopen("temp.dat","w");
   for (int i = 0; i < nbr_of_timesteps + 1; i++) {
     current_time = i * timestep;
     fprintf(energy_file, "%.4f \t %e \t %e \t %e \n", current_time, E_pot[i], E_kin[i], E_pot[i]+E_kin[i]);
-    fprintf(temperature_file, "%.4f \t %e\n", current_time, Temp[i]); 
   }
   fclose(energy_file);
-  fclose(temperature_file);
+  for (int i = 0; i < nbr_of_timesteps_eq + 1; i++) {
+  current_time = i * timestep;
+  fprintf(temp_file, "%.4f \t %e \n", current_time, temp[i]);
+  }
+  fclose(temp_file);
    
   /* Free allocated memory */
   gsl_rng_free(q); 
@@ -126,6 +151,8 @@ int main()
   free(v);
   free(E_pot);
   free(E_kin);
+  free(E_kin_eq);
   free(F);
-  free(Temp);
+  free(temp);
+
 }
