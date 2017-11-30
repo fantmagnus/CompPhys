1a32aa251a834c4931093167e05da5af5105700e
Magnus Fant <fantm@f7203-18.f7203.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-30 18:14:36 +0100

    Energy calculation function added

--- H2b/MC_main.c
+++ H2b/MC_main.c
@@ -17,15 +17,19 @@ double norm(double r[3]) {
 
 double calc_E(double r1[3], double r2[3], double alpha) {
   double r12 = distance(r1, r2);
-  double r1_hat[3], r2_hat[3];
+  double r1_hat[3], r2_hat[3], r_hat[3], r[3];
+  double scal; 
   for(int i = 0; i < 3; i++ ) {
     r1_hat[i] = r1[i]/norm(r1);
-    r2_hat[i] = r2[i]/norm(r2); 
+    r2_hat[i] = r2[i]/norm(r2);
+    r_hat[i] = r1_hat[i] - r2_hat[i];
+    r[i] = r1[i] - r2[i];
+    scal = r_hat[i]*r[i]; 
   }
   
-  double E_L = -4;
+  double E_L = -4 + scal/(r12*pow(1+alpha*r12,2)) - 1/(r12*pow(1+alpha*r12,3)) - 1/(4*pow(1+alpha*r12,4)) + 1/r12;
   
-  return 1;
+  return E_L;
 }
 
 double weightfunc(double r1[3], double r2[3], double alpha) {
