4485cee731ddd61c9d9b529191277e4ff01c3b03
Philip Edenborg <edenborg@f7204-17.f7204.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-12-05 10:04:54 +0100

    Just m-files.

--- H2b/energy.m
+++ H2b/energy.m
@@ -5,7 +5,14 @@ alpha = data(:,1);
 E = data(:,2);
 var_E = data(:,3);
 s=sqrt(var_E);
+
 errorbar(alpha,E,var_E,'r.','vertical')
 %xlim([0.125 0.175])
+xlabel('$\alpha$','interpreter','LaTeX')
+ylabel('$E$ [a.u.]','interpreter','LaTeX')
 
- 
+%%
+clc, close all
+data = importdata('E.dat');
+E = data(:,1);
+var(E)
--- H2b/error_estimate.m
+++ H2b/error_estimate.m
@@ -1,12 +1,34 @@
-clc, close all
+%% Correlation function
+clc, close all, clear all
 data = importdata('phi.dat');
 k=data(:,1);
 phi=data(:,2);
 
+figure(1)
 hold on
 plot(k,phi,'-')
 plot([min(k) max(k)],[exp(-2) exp(-2)],'-r')
 xlim([0 150])
 
+xlabel('k','interpreter','LaTeX')
+ylabel('Correlation function $\Phi_k$','interpreter','LaTeX')
+
+dim = [.5 .01 .5 .5];
+str = 'exp(-2)';
+%annotation('textbox',dim,'String',str,'FitBoxToText','on');
+
 
 s = 2*sum(phi) % statistical inefficiency
+
+
+% s = 10.28 when using phi_k=s=exp
+
+%% Block method
+data = importdata('S.dat');
+B = data(:,1);
+S = data(:,2);
+Blim=150;
+figure(2)
+plot(B(1:Blim),S(1:Blim),'.')
+xlabel('$B$','interpreter','LaTeX')
+ylabel('Statistical inefficiency $n_s$','interpreter','LaTeX')
