584cbccae9b2411ef9d9ea13eb5212202e57ad9f
philip.edenborg <philip.edenborg@gmail.com>
2018-01-04 13:05:16 +0100

    Test of reallocation

--- H3a/DMC_main.c
+++ H3a/DMC_main.c
@@ -20,6 +20,7 @@ double weight_func(double x, double E_T, double timestep) {
 
 int main() {
   /*Initializing variables */
+  int i,j,k;
   int N_0 = 300;
   int N = N_0;
   int timestep = 0.1;
@@ -27,10 +28,27 @@ int main() {
   double E_T = 2;
   double E_0 = 1/2;
 
+  double* walkers = malloc(4 * sizeof(double));
+
+  for (i = 0; i < 4; i++){
+    walkers[i] = i;
+  }
+
+  double* new_walkers = realloc(walkers, 5 * sizeof(double));
+  walkers = new_walkers;
+  walkers[4] = 5;
+
+  
+  for (i = 0; i < 5; i++){
+    printf("%f\n",walkers[i]);
+  }
+  
+  
+
   const gsl_rng_type*o;
   gsl_rng *q;
   gsl_rng_env_setup();
-  o = gsl_rng_degault;
+  o = gsl_rng_default;
   q = gsl_rng_alloc(o);
   gsl_rng_set(q, time(NULL));
   printf("");
